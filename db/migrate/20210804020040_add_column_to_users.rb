7fc8449d74092b8eef57c4c31dafba0e9262f7c4
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-04 11:27:23 +0900

    adminカラム:defaultをfalseに

--- db/migrate/20210804020040_add_column_to_users.rb
+++ db/migrate/20210804020040_add_column_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false, null: false
     add_column :users, :baby_due_on, :date
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_020040) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.date "baby_due_on"
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
