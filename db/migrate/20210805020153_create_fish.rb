5b525e478e471f1eeba4337c1b99370cb92c3da3
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-05 11:16:25 +0900

    fishesテーブル作成

--- db/migrate/20210805020153_create_fish.rb
+++ db/migrate/20210805020153_create_fish.rb
@@ -1,7 +1,13 @@
 class CreateFish < ActiveRecord::Migration[5.2]
   def change
     create_table :fish do |t|
-
+      t.string :name
+      t.string :line
+      t.float :n3_oil
+      t.float :vitamin_a
+      t.float :mercury
+      t.text :content
+      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_020040) do
+ActiveRecord::Schema.define(version: 2021_08_05_020153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fish", force: :cascade do |t|
+    t.string "name"
+    t.string "line"
+    t.float "n3_oil"
+    t.float "vitamin_a"
+    t.float "mercury"
+    t.text "content"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
