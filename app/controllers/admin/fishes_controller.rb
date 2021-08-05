0ff028136f09527159aafb0da085998e97149359
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-05 12:59:36 +0900

    admin用fishes_controller作成

---
+++ app/controllers/admin/fishes_controller.rb
@@ -0,0 +1,2 @@
+class Admin::FishesController < ApplicationController
+end
---
+++ test/controllers/admin/fishes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::FishesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
