b57a289f4f7bc5f25d1666be256877700718a02b
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-06 21:23:03 +0900

    アプリの使い方ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'tops#index'
   devise_for :users
+  get '/tops/help', to: 'tops#help'
   resources :users, only: [:show]
   resources :diaries, only: [:index]
   namespace :admin do
