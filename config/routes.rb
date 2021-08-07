479bb3f0cb7e6409e591245e13e0344ae1d0722b
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-07 11:14:12 +0900

    ユーザー情報編集・削除機能 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'tops#index'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+   }
   get '/tops/help', to: 'tops#help'
   resources :users, only: [:show]
   resources :diaries, only: [:index]
   namespace :admin do
     resources :fishes
-    resources :users
+    resources :users, only: [:index]
   end
   namespace :api do
     resources :fishes, only: [:index]
