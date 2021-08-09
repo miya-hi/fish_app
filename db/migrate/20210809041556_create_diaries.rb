e2ccdf4de5cf10f3090f0c9edd1310cc7ff4c4e1
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-09 13:26:20 +0900

    diariesテーブル作成

--- db/migrate/20210809041556_create_diaries.rb
+++ db/migrate/20210809041556_create_diaries.rb
@@ -1,7 +1,10 @@
 class CreateDiaries < ActiveRecord::Migration[5.2]
   def change
     create_table :diaries do |t|
-
+      t.integer :amount
+      t.datetime :input_at
+      t.references :user, null: false, foreign_key: true
+      t.references :fish, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_020153) do
+ActiveRecord::Schema.define(version: 2021_08_09_041556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "diaries", force: :cascade do |t|
+    t.integer "amount"
+    t.datetime "input_at"
+    t.bigint "user_id", null: false
+    t.bigint "fish_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fish_id"], name: "index_diaries_on_fish_id"
+    t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
 
   create_table "fish", force: :cascade do |t|
     t.string "name"
@@ -47,4 +58,6 @@ ActiveRecord::Schema.define(version: 2021_08_05_020153) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diaries", "fish"
+  add_foreign_key "diaries", "users"
 end
