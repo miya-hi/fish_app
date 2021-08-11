2a0877a75a058ed5a88f41a8d4281dc15fcd0662
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-11 12:37:48 +0900

    魚の登録後、詳細ページへ飛ばす

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,18 +1,31 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_diary, only: [:show, :edit, :update, :destroy]
+  before_action :set_diary, only: [:edit, :update, :destroy]
+
   def index
     @diaries = Diary.all
   end
+
   def new
     @diary = Diary.new
     @fishes = Fish.all
   end
+
   def create
     @diary = current_user.diaries.build(diary_params)
+    @diary.input_at = Date.today if @diary.input_at.blank?
+    @diary.save!
+    redirect_to diary_path(@diary.input_at.strftime('%Y-%m-%d')), notice: "作成しました" and return
   end
+
+  def show
+    # idには YYYY-MM-DD の日付が入ってくる
+    @diaries = current_user.diaries.where(input_at: params[:id]).order(id: :asc)
+  end
+
   def edit
   end
+
   def update
     if @diary.update(diary_params)
       redirect_to diaries_path, notice:"編集しました"
@@ -20,9 +33,16 @@ class DiariesController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @diary.destroy
+    redirect_to diaries_path, notice:"削除しました"
+  end
+
   private
+
   def diary_params
-    params.require(:diary).permit(:user_id, :fish_id, :amount, :input_at)
+    params.require(:diary).permit(:fish_id, :amount, :input_at)
   end
   def set_diary
     @diary = Diary.find(params[:id])
