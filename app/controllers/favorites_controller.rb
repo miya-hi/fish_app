c3db405f0f1ce94166081ad4e0df41e585a87c04
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-12 15:20:10 +0900

    エラー修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,10 +4,10 @@ class FavoritesController < ApplicationController
   end
   def create
     favorite = current_user.favorites.create(fish_id: params[:fish_id])
-    redirect_to new_diaries_path, notice: "#{favorite.fish.}をお気に入り登録しました"
+    redirect_to new_diary_path, notice: "#{favorite.fish.name}をお気に入り登録しました"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to new_diaries_path, notice: "#{favorite.fish.name}のお気に入りを解除しました"
+    redirect_to new_diary_path, notice: "#{favorite.fish.name}のお気に入りを解除しました"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :diaries, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_fishes, through: :favorites, source: :fish
 
   def email_required?
     false
