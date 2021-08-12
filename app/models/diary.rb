ae3b72bd4132120257da20eb7deec59d9b79fb7d
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-12 19:29:10 +0900

    タイムゾーン修正

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,9 +1,13 @@
 class Diary < ApplicationRecord
   belongs_to :user
   belongs_to :fish
-  
+
+  def self.default_timezone
+    :utc
+  end
+
   def start_time
-  input_at
+    input_at
   end
 
   def end_time
