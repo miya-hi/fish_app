d130c63e65324a9610ccb309c220970ec6ee5970
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-13 19:20:52 +0900

    amountの値に応じて計算結果が表示されるよう修正

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,9 +1,22 @@
 class Diary < ApplicationRecord
+  validates :amount, presence: true
   belongs_to :user
   belongs_to :fish
 
   def self.default_timezone
     :utc
+  end
+
+  def fish_n3_oil
+    amount * fish.n3_oil/100
+  end
+
+  def fish_mercury
+    amount * fish.mercury.to_f/100
+  end
+
+  def fish_vitamin_a
+    amount * fish.vitamin_a/100
   end
 
   def start_time
