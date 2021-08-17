a5662a2755b72c10c9e625463b74ac8364bd1bd1
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-17 19:28:57 +0900

    出産予定日:280日以内のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: {minimum:5, maximum: 20}
+  validate :pregnancy
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   has_many :diaries, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_fishes, through: :favorites, source: :fish
@@ -9,19 +11,27 @@ class User < ApplicationRecord
   def email_required?
     false
   end
+
   def email_changed?
     false
   end
+
   def self.guest
     find_or_create_by!(name: 'ゲスト001') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.baby_due_on = '2021-12-01'
     end
   end
+
   def self.admin_guest
     find_or_create_by!(name: '管理者001', admin: true) do |user|
       user.password = SecureRandom.urlsafe_base64
       user.baby_due_on = '2021-12-01'
     end
+  end
+
+  def pregnancy
+    errors.add(:baby_due_on, "は280日以降の登録ができません")unless
+    self.baby_due_on > self.baby_due_on+280
   end
 end
