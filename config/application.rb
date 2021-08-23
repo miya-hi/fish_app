617a925e88e195e1b7905e91ef533821870bce05
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-23 16:15:56 +0900

    Rspecの準備

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,16 @@ module FishApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.test_framework :rspec,
+        model_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
     Groupdate.time_zone = "Tokyo"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
