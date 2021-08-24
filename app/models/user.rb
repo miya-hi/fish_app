49b31c6a6be9be4b9c51867bb1bbac3e76f88c04
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-24 12:12:16 +0900

    パスワードのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: {minimum:5, maximum: 20}
+  validates :password, presence: true, length: {minimum:6}
   validate :pregnancy
 
   devise :database_authenticatable, :registerable,
