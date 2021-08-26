ffb9818cf1dc681faa6c95a526600c2d3b324347
miyazaki hitomi <hitomikirarin0830m@yahoo.co.jp>
2021-08-26 15:52:53 +0900

    魚の行：1文字のみ許可するバリデーション設定、例をプレースホルダーに記載

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,6 +1,6 @@
 class Fish < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :line, presence: true
+  validates :line, presence: true, length: {is: 1}
   validates :n3_oil, presence: true, numericality: { greater_than_or_equal_to: 0}
   validates :vitamin_a, presence: true, numericality: { greater_than_or_equal_to: 0}
   validates :mercury, numericality: { greater_than_or_equal_to: 0, allow_blank: true}
